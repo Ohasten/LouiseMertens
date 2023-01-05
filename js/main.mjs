850c510103f10c0db00f0f5d6c9f019b68842f5c
Fabian Thorsten Hassel <Fabian.hassel@gmx.de>
2023-01-05 15:06:22 +0100

    Add files via upload

--- js/main.mjs
+++ js/main.mjs
@@ -1,7 +1,8 @@
-import { comMain } from "./com.mjs";
-import { artMain } from "./art.mjs";
 import { navMain } from "./nav.mjs";
+import { artMain } from "./art.mjs";
+import { comMain } from "./com.mjs";
 import { infoMain } from "./info.mjs";
+import { aniMain } from "./animations.mjs";
 
 const API_TOKEN = "0e52eec14f3689141de9a1c48cf6572e292eb3bc18c872f1c2a41c6cbb8eddd50a1549a634e420563c7b960b01398577add350a75de77f17ced99e48d47c600da20d52e123e39707568efb1ac64a2380ce0a540c5a3c4f18cb0e1494f8770860d08b1996c96f8a4ba421b68210609cdf68140ef0f71ef87c1adbe8fda65116e2";
 const API_URL = "https://louise-mertens-cms.kolbgrafik.de/";
@@ -38,25 +39,6 @@ var initArtworks = function() {
 }
 var initCommissions = function () {
     comMain(commissions);
-
-    //Init Splide
-    (function(){
-        document.addEventListener( 'DOMContentLoaded', function() {
-            var splide = new Splide( '.splide', {});
-            splide.mount();
-        });
-    
-        var elms = document.getElementsByClassName( 'splide' );
-        for ( var i = 0; i < elms.length; i++ ) {
-            new Splide( elms[ i ] ).mount();
-        }
-    
-        Splide.defaults = {
-            type: 'loop',
-            pagination: false,
-        }
-    })()
-
 }
 var initInfo = function() {
     infoMain(informations);
@@ -64,12 +46,20 @@ var initInfo = function() {
 var initNav = function() {
     navMain();
 }
+var initAni = function() {
+    aniMain();
+}
 
 function main() {
     initArtworks();
-    initCommissions();
     initInfo();
     initNav();
+    initCommissions();
+
+    $(document).ready(function () {
+        initAni()
+    });
+    
 }
 
 main();
